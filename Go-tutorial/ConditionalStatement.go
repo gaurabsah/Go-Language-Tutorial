70b0a1a1254b1693ba1b4ffb01aba8336e3a37ef
Gaurab Sah <gaurab.sah.777@gmail.com>
2022-12-25 13:29:45 +0530

    if-else

---
+++ Go-tutorial/ConditionalStatement.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	const age int = 17
+	if age >= 18 {
+		fmt.Println("You are welcome to the club")
+	} else {
+		fmt.Println("You are not welcome")
+	}
+
+}
